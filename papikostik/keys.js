e5caf85d7fba486cad496489a53da6c865b7db3c
Huda Prasetyo <huda.prs2002@gmail.com>
2020-09-11 14:52:50 +0700

    completing report and add papikostik

---
+++ papikostik/keys.js
@@ -0,0 +1,24 @@
+const orederedKeys = [
+  "n",
+  "g",
+  "a",
+  "l",
+  "p",
+  "i",
+  "t",
+  "v",
+  "x",
+  "s",
+  "b",
+  "o",
+  "r",
+  "d",
+  "c",
+  "z",
+  "e",
+  "k",
+  "f",
+  "w"
+]
+const bottomGroup = ["n", "a", "p", "x", "b", "o", "z", "k", "f", "w"]
+const topGroup = ["g", "l", "i", "t", "v", "s", "r", "d", "c", "e"]
